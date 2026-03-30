ae4f3f7ffe95a8319f044520590a5aaae7f9fdce
GamerBoi801 <geeseplayer@gmail.com>
2026-03-30 16:41:51 +0500

    matrix class implementated

--- matrix.h
+++ matrix.h
@@ -10,25 +10,28 @@ class Matrix {
 // constructor
     Matrix (int r, int c) {
         rows =r; cols=c;
+        // ini the empty vector
+        vector<vector<double>>(rows, vector<double>(cols, 0.0));
+        
     }
     // transposes the matrix
-    void transpose() {
+    Matrix  transpose() {
+        Matrix C(cols, rows); ; //changes dimension from (rows, cols) -> (cols, rows)
         vector<vector<double>> copy(cols, vector<double>(rows));
         for (int i = 0; i < cols; i++) {
             for(int j =0 ; j < rows;j++) {
-                copy[i][j] = matrix[j][i];
+                C.matrix[i][j] = matrix[j][i];
             }
             
         }
-        matrix = copy;
-        swap(rows, cols); //changes dimension from (rows, cols) -> (cols, rows)
+        return C;
     }
 
     void print_matrix() {
         for(int i =0 ; i < rows; i++) {
             for(int j =0; j < cols; j++){
                 if (j == 0) cout << matrix[i][j];
-                cout << matrix[i][j] << ", ";
+                else cout << matrix[i][j] << ", ";
             }
             cout << endl;
         }
@@ -49,33 +52,53 @@ class Matrix {
                 r.matrix[i][j] = (matrix[i][j] + B.matrix[i][j]);
             }
         }
+        return r;
 
     }
 
      // performs subtraction between 2 matrices
-    Matrix subtraction(Matrix & B) {
-        Matrix r(rows, cols);
+    Matrix subtract(Matrix & B) {
+        Matrix C(rows, cols);
         // checking for the same dimensions
         if (!(rows == B.rows && cols == B.cols)) {
-            cout << "Invalid Operation: Dimensions of both the matrices r not the same" << endl;
-            return r;
+            cout << "Invalid Operation: Dimensions of both the matrices C not the same" << endl;
+            return C;
         }
         for(int i = 0 ; i < rows; i ++ ) {
             for(int j =0; j < cols; j++) {
-                r.matrix[i][j] = (matrix[i][j] - B.matrix[i][j]);
+                C.matrix[i][j] = (matrix[i][j] - B.matrix[i][j]);
             }
         }
-
+        return C;
     }
+    // matrix multiplication
     Matrix matmul(Matrix & B) {
         // performs matrix multiplication
         // valid operation when cols of A are equal to rows of B 
-        Matrix r(rows, B.cols); // resultant matrix
+        Matrix C(rows, B.cols); // resultant matrix
         if (!(cols == B.rows)){
             cout << "Invalid Operation: This is not a valid operation as A.columns != B.rows" << endl;
+            return C;
         }
 
+        for(int i = 0; i < C.rows;i++) {
+            for(int j = 0; j < C.cols; j++) {
+                for(int k = 0 ; k < cols; k++) {
+                    C.matrix[i][j] += matrix[i][k] * B.matrix[k][j];
+                }
+            }
 
+        }
+        return C;
+    }
+
+    Matrix scale(double alpha) {
+        Matrix C(rows, cols);
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0 ; j < cols; j++ ) {
+                C.matrix[i][j] = alpha * matrix[i][j];
+            }
+        }
     }
 
 };
