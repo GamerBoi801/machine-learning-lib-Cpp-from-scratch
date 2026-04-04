9aa2617d61b3ef27cb530dcb0d78d89128d74391
GamerBoi801 <geeseplayer@gmail.com>
2026-04-04 10:32:24 +0500

    dataset constructor

--- dataset.h
+++ dataset.h
@@ -1,8 +1,13 @@
-
- #include <iostream>
- using namespace std;
-
+#include "matrix.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
 
 class Dataset {
-    
+    public:
+    Matrix X; // features[m x n] becomes [m x n+1] after bias
+    Matrix Y; // targets [m x 1]
+    int m; // number of samples
+    int n; // number of features
+
 };
